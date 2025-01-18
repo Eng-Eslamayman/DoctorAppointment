87472f3af872d3a8ca5cf0d1d6c56ad304da264e
mhmdElnagar <m.elnagar@ai-techno.org>
2025-01-18 10:38:52 +0200

    mergre to repo

---
+++ TraditionalService/Implementations/NotificationService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TraditionalData;
+
+namespace TraditionalService.Implementations
+{
+    public class NotificationService
+    {
+        public void SendConfirmation(Appointment appointment)
+        {
+            Console.WriteLine($"Confirmation: Appointment for {appointment.PatientName} with Doctor at {appointment.ReservedAt}.");
+        }
+    }
+
+}
---
+++ TraditionalService/Repository Layer_In-Memory/InMemorySlotRepository.cs
@@ -0,0 +1,26 @@
+﻿using Service.Repository_Layer_In_Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TraditionalData;
+
+namespace TraditionalService.Repository_Layer_In_Memory
+{
+    public class InMemorySlotRepository : ISlotRepository
+    {
+        private readonly List<Slot> _slots = new List<Slot>();
+
+        public IEnumerable<Slot> GetAvailableSlots()
+        {
+            return _slots.Where(s => !s.IsReserved);
+        }
+
+        public Slot AddSlot(Slot slot)
+        {
+            _slots.Add(slot);
+            return slot;
+        }
+    }
+}
